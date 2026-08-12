64103da083a4da4050ab7b91e9502bd4e972811e
nazmul-204 <azkiller006@gmail.com>
2026-08-12 13:18:50 +0600

    Delete BackgroundFX.jsx

--- BackgroundFX.jsx
+++
@@ -1,9 +0,0 @@
-export default function BackgroundFX() {
-  return (
-    <div className="bg-fx" aria-hidden="true">
-      <span className="bg-fx__blob bg-fx__blob--gold" />
-      <span className="bg-fx__blob bg-fx__blob--amber" />
-      <span className="bg-fx__grid" />
-    </div>
-  )
-}
